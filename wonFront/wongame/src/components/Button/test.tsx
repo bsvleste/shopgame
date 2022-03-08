7b8a949b415504732af21c8fc806b2d4b2c3014b
Bruno <bvaleiro@gmail.com>
2022-03-08 11:55:48 -0300

    finalizando o button

---
+++ wonFront/wongame/src/components/Button/test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import { renderWhitTheme } from 'utils/test/helpers';
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart';
+import Button from '.';
+
+describe('Button', () => {
+  it('should render medium size bottom by default ', () => {
+    const { container } = renderWhitTheme(<Button>Buy Now</Button>);
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    });
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it('should render small size bottom when passed ', () => {
+    renderWhitTheme(<Button size="small">Buy Now</Button>);
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    });
+  });
+  it('should render large size bottom when passed ', () => {
+    renderWhitTheme(<Button size="large">Buy Now</Button>);
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      padding: '0.8rem 4.8rem',
+      height: '5rem',
+      'font-size': '1.6rem'
+    });
+  });
+  it('should render a fullwidth version ', () => {
+    renderWhitTheme(<Button fullWidth>Buy Now</Button>);
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%'
+    });
+  });
+  it('should render a icon version ', () => {
+    renderWhitTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy Now</Button>
+    );
+    expect(screen.getByText(/Buy now/i)).toBeInTheDocument();
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
+  });
+});
